c0c805b9cbb6ea80b0559f4d42295fb3d26c0d09
lukeptomey <55600217+lukeptomey@users.noreply.github.com>
2020-08-15 12:19:16 -0600

    Update main.js
    help command added

--- main.js
+++ main.js
@@ -2,8 +2,22 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-client.once('ready' , () =>{
+const prefix = '-';
+
+
+client.once('ready' , () => {
     console.log('TrainBot is online');
+});
+
+client.on('message', message=>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'help'){
+        message.channel.send('GitBot \nName: TrainBot \n"-" is prefix for commands \n"-help" gives help guide');
+    }
 });
 
 client.login('');
